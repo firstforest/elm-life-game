2af45ac7bb3a1d879272dcb5988af033daa05e12
Hajime Morikoshi <h.morikosi@gmail.com>
2014-10-12 15:46:36 +0900

    マウスクリックでセルを作るようにする

--- Main.elm
+++ Main.elm
@@ -1,64 +1,86 @@
 import Dict as D
+import Mouse
+import Debug
 
-size = 30
+size = 20
 
-type Cells = D.Dict (Float, Float) Bool
+type Cell = { isAlive:Bool, isSelected:Bool }
+type Cells = D.Dict (Float, Float) Cell
+type Input = {x:Int, y:Int, delta:Float, isDown:Bool}
 
 xs = concat (map (repeat size) [0..(size - 1)])
 ys = concat (repeat size [0..(size - 1)])
 positionList = zip xs ys
 
 initialCells : Cells
-initialCells = D.fromList (map (\p -> (p, False)) positionList)
+initialCells = D.fromList (map (\p -> (p, Cell False False)) positionList)
 
 insertCell : [(Float, Float)] -> Cells -> Cells
 insertCell ps cs =
   let
-    i (x, y) ccs = D.insert (x, y) True ccs
+    i (x, y) ccs = D.insert (x, y) (Cell True False) ccs
   in
     foldl i cs ps
 
 testCells = insertCell [(4,4), (4,5), (4,6), (4,7),(4,8)] initialCells
 
-getAroundCells : (Float, Float) -> Cells -> [Bool]
+falseCell = Cell False False
+
+getAroundCells : (Float, Float) -> Cells -> [Cell]
 getAroundCells (x, y) cs =
-  [ D.getOrElse False (x-1,y-1) cs
-  , D.getOrElse False (x-1,y) cs
-  , D.getOrElse False (x-1,y+1) cs
-  , D.getOrElse False (x,y-1) cs
-  , D.getOrElse False (x,y+1) cs
-  , D.getOrElse False (x+1,y-1) cs
-  , D.getOrElse False (x+1,y) cs
-  , D.getOrElse False (x+1,y+1) cs
+  [ D.getOrElse falseCell (x-1,y-1) cs
+  , D.getOrElse falseCell (x-1,y) cs
+  , D.getOrElse falseCell (x-1,y+1) cs
+  , D.getOrElse falseCell (x,y-1) cs
+  , D.getOrElse falseCell (x,y+1) cs
+  , D.getOrElse falseCell (x+1,y-1) cs
+  , D.getOrElse falseCell (x+1,y) cs
+  , D.getOrElse falseCell (x+1,y+1) cs
   ]
 
 stepCell : Cells -> (Float, Float) -> Cells -> Cells
 stepCell base (x, y) cs =
   let
     aroundCells = getAroundCells (x, y) base
-    aliveNums = length (filter identity aroundCells)
-    b = (2 == aliveNums && D.getOrElse False (x, y) cs) || aliveNums == 3
+    aliveNums = length (filter (\c -> c.isAlive) aroundCells)
+    b = (2 == aliveNums && .isAlive (D.getOrElse falseCell (x, y) cs)) || aliveNums == 3
   in
-    D.insert (x, y) b cs
+    D.insert (x, y) (Cell b False) cs
 
-stepCells : a -> Cells -> Cells
-stepCells _ cells =
-  foldl (stepCell cells) cells (D.keys cells)
+stepCells : Input -> Cells -> Cells
+stepCells {x, y, delta, isDown} cells =
+  let
+    nextCells =
+      if (floor (Debug.watch "delta" delta)) % 30 == 0
+      then foldl (stepCell cells) cells (D.keys cells)
+      else D.map (\c -> {c | isSelected <- False }) cells
+    pos = (toFloat (x // cellsize), toFloat (y // cellsize))
+    selectedCell = D.getOrElse falseCell pos nextCells
+  in
+    D.insert pos {selectedCell | isSelected <- True, isAlive <- selectedCell.isAlive || isDown} nextCells
 
-cellsize = 20
+cellsize = 10
 offset = (size*cellsize/2) - 5
 
-createCell : ((Float, Float),Bool) -> Form
-createCell ((x, y),b) =
+cellColor cell =
+  if cell.isSelected 
+  then red
+  else 
+   if cell.isAlive then black else gray
+
+createCell : ((Float, Float),Cell) -> Form
+createCell ((x, y),cell) =
   let
     fillSize = cellsize - 2
   in
     rect fillSize fillSize |>
-    filled (if b then black else gray) |>
+    filled (cellColor cell) |>
     move (-offset + (x * cellsize), offset - (y * cellsize))
 
 display : Cells -> Element
 display cells =
   map createCell (D.toList cells) |> collage (cellsize * size) (cellsize * size)
 
-main = lift display <| foldp stepCells testCells (every second)
+input = Input <~ Mouse.x ~ Mouse.y ~ foldp (+) 0 (sampleOn (fps 30) (constant 1)) ~ Mouse.isDown
+
+main = lift display <| foldp stepCells testCells input
